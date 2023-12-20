1d3855240fbc79786792c23843fd037f3e6e57a5
MdRehanAli <rehanali21d>
2023-12-20 19:33:08 +0600

    added views section add some changes

--- server.js
+++ server.js
@@ -7,9 +7,34 @@ function something() {
     console.log("Called.")
 
 }
-app.get("/", () => {
-    console.log("Called Again.")
+// app.get("/", () => {
+//     console.log("Called Again.")
+// })
+
+app.get("/", (req, res) => {
+    // console.log(req)
+    res.send("<h1>Home Page<h1>")
+})
+
+app.get("/about", (req, res) => {
+    // console.log(req)
+    res.send("<h1>About Page<h1>")
+})
+
+app.get("/contact", (req, res) => {
+    // console.log(req)
+    res.send("<h1>Contact Page<h1>")
+})
+
+app.get("/login", (req, res) => {
+    // console.log(req)
+    res.send("<h1>Login Page<h1>")
+})
+
+app.get("/registration", (req, res) => {
+    // console.log(req)
+    res.send("<h1>Registration Page<h1>")
 })
 
 app.listen(3000)
-localhost:3000
+localhost: 3000
